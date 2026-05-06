e749fff742237ecdb986c2b8cabf16c69c5ff755
muzi-1019 <li13395400749@outlook.com>
2026-05-06 08:24:49 +0800

    1

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.core.security import create_access_token, hash_password, verify_password
 
 
